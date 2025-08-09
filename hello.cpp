eba544b351f023b03707ad29697a74ab56ffca26
404 not found <124446532+Yang-Zhikun@users.noreply.github.com>
2025-08-09 21:17:36 +0800

    update normally

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,13 @@
 #include<iostream>
+#include "headers/header.h"
 using namespace std;
 int main() {
     cout << "Hello World!" << endl;
     cout << "ÄãºÃ£¬ÊÀ½ç£¡" << endl;
+    int a;
+    damn obj;
+    obj.sayHello();
+    cout << "\n";
     system("pause");
     return 0;
 }
