6f2f7632088ccb91e5cc355b67242cf542ff71eb
404 not found <776560548@qq.com>
2025-10-26 16:29:34 +0800

    merge

--- hello-world.cpp
+++ hello-world.cpp
@@ -3,6 +3,9 @@ using namespace std;
 int main() {
     cout << "Hello World!" << endl;
     cout << "你好，世界！" << endl;
+    int i = 0;
+    cin >> i;
+    cout << "You entered: " << i << endl;
     system("pause");
     return 0;
 }
