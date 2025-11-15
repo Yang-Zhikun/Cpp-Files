538b63321a5a8936c699de709bb189d5181ea186
404 not found <776560548@qq.com>
2025-11-16 07:49:35 +0800

    update

--- hello-world.cpp
+++ hello-world.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main() {
     cout << "Hello World!" << endl;
-    cout << "你好，世界！" << endl;
+    cout << "你好，世界！" << endl << "请输入一个整数：";
     int i = 0;
     cin >> i;
     cout << "You entered: " << i << endl;
